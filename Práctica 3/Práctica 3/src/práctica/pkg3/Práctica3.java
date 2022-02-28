f3341e800481b1b2d7381b6bba8b06400411adc9
Luis Galindo <egalindo54@uabc.edu.mx>
2022-02-28 12:59:05 -0800

    Estudiantes creado

---
+++ Práctica 3/Práctica 3/src/práctica/pkg3/Práctica3.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package práctica.pkg3;
+
+import java.util.Scanner;
+import java.util.Vector;
+
+/**
+ *
+ * @author galindo
+ */
+public class Práctica3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Vector<Estudiante> registroEstudiantes = new Vector<>();
+        
+        registroEstudiantes.add(new Estudiante("0", "Luis", "Historia", 60));
+        registroEstudiantes.add(new Estudiante("1", "Mario", "Matematicas", 95));
+        registroEstudiantes.add(new Estudiante("2", "Pedro", "Ciaencias", 76));
+        registroEstudiantes.add(new Estudiante("3", "Fransisco", "Calculo", 89));
+        registroEstudiantes.add(new Estudiante("4", "Aldo", "Quimica", 45));
+
+
+        
+        System.out.println("Menú --------------\n");
+        System.out.println("a. Capturar alumnos");
+        System.out.println("b. Mostrar alumnos");
+        System.out.println("c. Salir");
+        
+    }
+    
+}
