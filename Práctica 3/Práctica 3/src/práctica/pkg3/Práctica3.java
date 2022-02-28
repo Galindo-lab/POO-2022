b7495eb60ce63874de088090e57ec3714dc0e87b
Luis Galindo <egalindo54@uabc.edu.mx>
2022-02-28 13:03:06 -0800

    menu agregaod

--- Práctica 3/Práctica 3/src/práctica/pkg3/Práctica3.java
+++ Práctica 3/Práctica 3/src/práctica/pkg3/Práctica3.java
@@ -20,20 +20,39 @@ public class Práctica3 {
     public static void main(String[] args) {
 
         Vector<Estudiante> registroEstudiantes = new Vector<>();
-        
+        Scanner sc = new Scanner(System.in);
+        char op = ' ';
+
         registroEstudiantes.add(new Estudiante("0", "Luis", "Historia", 60));
         registroEstudiantes.add(new Estudiante("1", "Mario", "Matematicas", 95));
         registroEstudiantes.add(new Estudiante("2", "Pedro", "Ciaencias", 76));
         registroEstudiantes.add(new Estudiante("3", "Fransisco", "Calculo", 89));
         registroEstudiantes.add(new Estudiante("4", "Aldo", "Quimica", 45));
 
+        do {
 
-        
-        System.out.println("Menú --------------\n");
-        System.out.println("a. Capturar alumnos");
-        System.out.println("b. Mostrar alumnos");
-        System.out.println("c. Salir");
-        
+            System.out.println("Menú --------------\n");
+            System.out.println(" a. Capturar alumnos");
+            System.out.println(" b. Mostrar alumnos");
+            System.out.println(" c. Salir");
+
+            op = sc.next().charAt(0);
+            
+            switch(op) {
+                case 'a':
+                    break;
+                    
+                case 'b':
+                    break;
+                    
+                case 'c':
+                    break;
+                    
+                default:
+                    break;
+            }
+
+        } while (op != 'c');
     }
-    
+
 }
