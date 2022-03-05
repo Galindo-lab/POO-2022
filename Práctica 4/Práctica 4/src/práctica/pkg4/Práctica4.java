8a0587539d4e79758034f10b85f5d3169ba09536
Luis Galindo <egalindo54@uabc.edu.mx>
2022-03-05 11:07:01 -0800

    Primer Commit

---
+++ Práctica 4/Práctica 4/src/práctica/pkg4/Práctica4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package práctica.pkg4;
+
+/**
+ *
+ * @author galindo
+ */
+public class Práctica4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
