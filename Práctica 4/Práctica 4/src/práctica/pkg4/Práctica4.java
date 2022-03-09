3b0f279d19bda364b9381addb72d3026f50d0d6d
Luis Galindo <egalindo54@uabc.edu.mx>
2022-03-08 23:36:02 -0800

    Terminado

--- Práctica 4/Práctica 4/src/práctica/pkg4/Práctica4.java
+++ Práctica 4/Práctica 4/src/práctica/pkg4/Práctica4.java
@@ -5,6 +5,9 @@
  */
 package práctica.pkg4;
 
+import java.text.ParseException;
+import java.util.Scanner;
+
 /**
  *
  * @author galindo
@@ -14,8 +17,47 @@ public class Práctica4 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        char op;
+        
+        Mexicano m = new Mexicano(sc);
+        
+        do{
+            System.out.println(" 1. Capurar");
+            System.out.println(" 2. CURP ");
+            System.out.println(" 3. RFC");
+            System.out.println(" 4. Informacion");
+            System.out.println("-----------------");
+            System.out.println(" 0. salir");
+            
+            op = sc.next().charAt(0);
+            
+            switch(op) {
+                case '1':
+                    m = new Mexicano(sc);
+                    break;
+                    
+                case '2':
+                    System.out.println(Curp.generar(m));
+                    break; 
+                    
+                case '3':
+                    System.out.println(RFC.generar(m));
+                    break;
+                   
+                case '4':
+                    System.out.println(m.toString());
+                    break;
+            }
+            
+        }while(op != '0');
+        
+        //Mexicano v = new Mexicano("LUIS Fransisco ROMERO PLAZUELOS", 'H', "01.12.1995", "BS", "mexicano");
+        //System.out.println(v.toString());
+        //System.out.println(Curp.generar(v));
+        //System.out.println(RFC.generar(v));
     }
     
 }
