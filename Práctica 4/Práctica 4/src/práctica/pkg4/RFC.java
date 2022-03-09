5a3715e20d7a48c9babcf279fce1c87cdfb07692
Luis Galindo <egalindo54@uabc.edu.mx>
2022-03-08 23:36:19 -0800

    Terminado

---
+++ Práctica 4/Práctica 4/src/práctica/pkg4/RFC.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package práctica.pkg4;
+
+/**
+ *
+ * @author galindo
+ */
+public class RFC {
+    
+    public static String generar(Mexicano foo) {
+        return Curp.generar(foo).substring(0, 10);
+    }
+}
